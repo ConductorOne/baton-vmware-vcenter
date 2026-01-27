501a4d3c45c196963f44dcef134e0d1b4c0b1bbf
Steve Gontzes <gontzess@gmail.com>
2026-01-27 17:10:36 -0500

    refactor: use generated config type instead of viper
    Replace viper config with SDK generated config type VmwareVcenter.

--- cmd/baton-vmware-vcenter/main.go
+++ cmd/baton-vmware-vcenter/main.go
@@ -11,10 +11,9 @@ import (
 	"github.com/conductorone/baton-sdk/pkg/connectorrunner"
 	"github.com/conductorone/baton-sdk/pkg/types"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
-	"github.com/spf13/viper"
 	"go.uber.org/zap"
 
-	vcenterconfig "github.com/conductorone/baton-vmware-vcenter/pkg/config"
+	cfg "github.com/conductorone/baton-vmware-vcenter/pkg/config"
 	"github.com/conductorone/baton-vmware-vcenter/pkg/connector"
 )
 
@@ -27,7 +26,7 @@ func main() {
 		ctx,
 		"baton-vmware-vcenter",
 		getConnector,
-		vcenterconfig.ConfigurationSchema,
+		cfg.Config,
 		connectorrunner.WithDefaultCapabilitiesConnectorBuilder(&connector.VMwareVCenter{}),
 	)
 	if err != nil {
@@ -44,29 +43,26 @@ func main() {
 	}
 }
 
-func getConnector(ctx context.Context, v *viper.Viper) (types.ConnectorServer, error) {
+func getConnector(ctx context.Context, c *cfg.VmwareVcenter) (types.ConnectorServer, error) {
 	l := ctxzap.Extract(ctx)
 
-	vcenterServerURL := v.GetString(vcenterconfig.VCenterServerURLField.FieldName)
-	insecure := v.GetBool(vcenterconfig.InsecureField.FieldName)
-
-	u, err := url.Parse(vcenterServerURL)
+	u, err := url.Parse(c.VcenterServerUrl)
 	if err != nil {
 		l.Error("error parsing vCenter server URL", zap.Error(err))
 		return nil, err
 	}
 
-	cb, err := connector.New(ctx, u, insecure)
+	cb, err := connector.New(ctx, u, c.Insecure)
 	if err != nil {
 		l.Error("error creating connector", zap.Error(err))
 		return nil, err
 	}
 
-	c, err := connectorbuilder.NewConnector(ctx, cb)
+	conn, err := connectorbuilder.NewConnector(ctx, cb)
 	if err != nil {
 		l.Error("error creating connector", zap.Error(err))
 		return nil, err
 	}
 
-	return c, nil
+	return conn, nil
 }
